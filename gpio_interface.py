c752554bd88624837fa1afaf49bcb60a8061cfb2
Ash McCoy <mccoa499@gmail.com>
2023-03-25 12:07:50 -0700

    Added a status indicator

--- gpio_interface.py
+++ gpio_interface.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 
 led = 17
 
+
+led_status = False
+
 # Set up GPIO pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
@@ -16,28 +19,42 @@ def show_user_profile(username):
 
 @app.route("/")
 def index():
+    global led_status
+    
     # Generate a chache buster based on the current time
     cache_buster = int(time.time())
-    # Render the index file and pass the cache buster to it
-    return render_template("index.html", cache_buster=cache_buster)
+    # Check the LED status
+    print(led_status)
+    if led_status:
+        status_string = "on"
+    else:
+        status_string = "off"
+    print(status_string)
+
+    # Render the index file and pass the cache buster and LED status to it
+    return render_template("index.html", cache_buster=cache_buster, status=status_string)
 
 @app.route("/on")
 def turn_on():
+    global led_status
+
     GPIO.output(led,GPIO.HIGH)
-    
+    led_status = True
     # Generate a chache buster based on the current time
     cache_buster = int(time.time())
     # Render the index file and pass the cache buster to it
-    return render_template("index.html", cache_buster=cache_buster)
+    return index()
 
 @app.route("/off")
 def turn_off():
+    global led_status
+
     GPIO.output(led,GPIO.LOW)
-    
+    led_status = False
     # Generate a chache buster based on the current time
     cache_buster = int(time.time())
     # Render the index file and pass the cache buster to it
-    return render_template("index.html", cache_buster=cache_buster)
+    return index()
 
 
 if __name__ == "__main__":
